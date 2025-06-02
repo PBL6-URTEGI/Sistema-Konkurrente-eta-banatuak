628b1be7335eb629e6b4c096adebbbe614e28a59
Amaia Bartolomé <amaiabart@gmail.com>
2025-06-02 11:56:43 +0200

    prueba

--- stream/src/main/java/stream/zona1/subscriber/KafkaConnector.java
+++ stream/src/main/java/stream/zona1/subscriber/KafkaConnector.java
@@ -17,7 +17,7 @@ public class KafkaConnector {
 
     public KafkaConnector() {
         factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("10.0.40.16");
 		factory.setUsername("rabbit");
 		factory.setPassword("rabbit");
     }
@@ -70,17 +70,17 @@ public class KafkaConnector {
             String message = new String(body, "UTF-8");
             System.out.println("[Bridge -> Kafka] " + message);
 
-            // Properties kafkaProps = new Properties();
-            // kafkaProps.put("bootstrap.servers", "localhost:9092");
-            // kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            // kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            Properties kafkaProps = new Properties();
+            kafkaProps.put("bootstrap.servers", "localhost:9092");
+            kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-            // KafkaProducer<String, String> producer = new KafkaProducer<>(kafkaProps);
+            KafkaProducer<String, String> producer = new KafkaProducer<>(kafkaProps);
 
-            // ProducerRecord<String, String> record = new ProducerRecord<>(KAFKA_TOPIC, message);
-            // producer.send(record);
-            // producer.flush();
-            // producer.close();
+            ProducerRecord<String, String> record = new ProducerRecord<>(KAFKA_TOPIC, message);
+            producer.send(record);
+            producer.flush();
+            producer.close();
         }
 
     }
